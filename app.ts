c96b947a73ebf219c61fd2bb47d5b663fd9b3723
stuartdambi@gmail.com <stuartdambi@gmail.com>
2022-03-16 18:31:08 +0300

    add heroku config

--- app.ts
+++ app.ts
@@ -11,6 +11,14 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/v1', wordRouter);
 
+app.get('/', (req: express.Request, res: express.Response) => {
+  return res.send(`
+  <center style="margin-top: 300px">
+    <h1>Section C: Code Challenge (Backend) - Stuart Dambi</h1>
+  </center>
+  `);
+});
+
 const PORT = process.env.PORT || 5500;
 
 app.listen(PORT, () => console.log(`app listening on port ${PORT}`));
